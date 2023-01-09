fc93601a0bc39c5622ff9f79724ccd6721872582
chenxi <907725402@qq.com>
2023-01-09 17:37:06 +0800

    优雅检验入参

--- src/main/java/com/imooc/mall/exception/GlobalExceptionHandler.java
+++ src/main/java/com/imooc/mall/exception/GlobalExceptionHandler.java
@@ -3,9 +3,15 @@ package com.imooc.mall.exception;
 import com.imooc.mall.common.ApiRestResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 描述：   处理统一异常的Handler
@@ -27,5 +33,28 @@ public class GlobalExceptionHandler {
     public Object handleImoocMallException(ImoocMallException e) {
         log.error("ImoocMallException: ", e);
         return ApiRestResponse.error(e.getCode(), e.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public ApiRestResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException: ", e);
+        return handleBindingResult(e.getBindingResult());
+    }
+
+    public ApiRestResponse handleBindingResult(BindingResult result) {
+        List<String> list = new ArrayList<>();
+
+        if (result.hasErrors()) {
+            List<ObjectError> allErrors = result.getAllErrors();
+            for (ObjectError objectError : allErrors) {
+                String message = objectError.getDefaultMessage();
+                list.add(message);
+            }
+        }
+        if (list.size() == 0) {
+            return ApiRestResponse.error(ImoocMallExceptionEnum.REQUEST_PARAM_ERROR);
+        }
+        return ApiRestResponse.error(ImoocMallExceptionEnum.REQUEST_PARAM_ERROR.getCode(), list.toString());
     }
 }
