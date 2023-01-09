e181e7bca61cb486b04059598e183fd8bf6c8fea
chenxi <907725402@qq.com>
2023-01-09 23:13:32 +0800

    统一校验管理员身份，代码优化

--- src/main/java/com/imooc/mall/controller/CategoryController.java
+++ src/main/java/com/imooc/mall/controller/CategoryController.java
@@ -3,10 +3,7 @@ package com.imooc.mall.controller;
 import com.imooc.mall.Service.CategoryService;
 import com.imooc.mall.Service.UserSerivce;
 import com.imooc.mall.common.ApiRestResponse;
-import com.imooc.mall.common.Constant;
-import com.imooc.mall.exception.ImoocMallExceptionEnum;
 import com.imooc.mall.model.pojo.Category;
-import com.imooc.mall.model.pojo.User;
 import com.imooc.mall.model.request.AddCategoryReq;
 import com.imooc.mall.model.request.UpdateCategoryReq;
 import io.swagger.annotations.Api;
@@ -18,7 +15,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 /**
@@ -37,43 +33,25 @@ public class CategoryController {
     @ApiOperation("后台添加目录")
     @PostMapping("admin/category/add")
     @ResponseBody
-    public ApiRestResponse addCategory(HttpSession session, @Valid @RequestBody AddCategoryReq addCategoryReq) {
-
-        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
-        if (currentUser == null) {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
-        }
-
-        //校验是否是管理员
-        boolean adminRole = userSerivce.checkAdminRole(currentUser);
-        if (adminRole) {
-            //是管理员执行操作
-            categoryService.add(addCategoryReq);
-            return ApiRestResponse.success();
-        } else {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
-        }
+    public ApiRestResponse addCategory(@Valid @RequestBody AddCategoryReq addCategoryReq) {
+        categoryService.add(addCategoryReq);
+        return ApiRestResponse.success();
     }
 
     @ApiOperation("后台更新目录")
     @PostMapping("admin/category/update")
     @ResponseBody
-    public ApiRestResponse updateCategory(@Valid @RequestBody UpdateCategoryReq updateCategoryReq, HttpSession session) {
-        User currentUser = (User) session.getAttribute(Constant.IMOOC_MALL_USER);
-        if (currentUser == null) {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_LOGIN);
-        }
+    public ApiRestResponse updateCategory(@Valid @RequestBody UpdateCategoryReq updateCategoryReq) {
+        Category category = new Category();
+        BeanUtils.copyProperties(updateCategoryReq, category);
+        categoryService.update(category);
+        return ApiRestResponse.success();
+    }
 
-        //校验是否是管理员
-        boolean adminRole = userSerivce.checkAdminRole(currentUser);
-        if (adminRole) {
-            //是管理员执行操作
-            Category category = new Category();
-            BeanUtils.copyProperties(updateCategoryReq, category);
-            categoryService.update(category);
-            return ApiRestResponse.success();
-        } else {
-            return ApiRestResponse.error(ImoocMallExceptionEnum.NEED_ADMIN);
-        }
+    @ApiOperation("后台删除目录")
+    @PostMapping("admin/category/delete")
+    @ResponseBody
+    public ApiRestResponse deleteCategory() {
+        return null;
     }
 }
