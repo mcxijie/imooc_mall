6711eed27dbbc7d576286ff9ae6f850cfea45eb5
chenxi <907725402@qq.com>
2023-01-10 17:07:24 +0800

    图片上传接口

--- src/main/java/com/imooc/mall/common/Constant.java
+++ src/main/java/com/imooc/mall/common/Constant.java
@@ -1,5 +1,7 @@
 package com.imooc.mall.common;
 
+import org.springframework.beans.factory.annotation.Value;
+
 /**
  * 描述：   常量值
  */
@@ -7,4 +9,7 @@ public class Constant {
 
     public static final String IMOOC_MALL_USER = "imooc_mall_user";
     public static final String SALT = "8svbsvjkweDF,.03[";
+
+    @Value("${file.upload.dir}")
+    public static String FILE_UPLOAD_DIR;
 }
